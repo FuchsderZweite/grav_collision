1facd5bc426e4b582aa46def97d6d318874e1430
sergej <email.sergej@web.de>
2021-03-02 13:38:47 +0100

    Wall collision detection added

--- main.py
+++ main.py
@@ -11,81 +11,98 @@ BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 clock = pygame.time.Clock()
 
-NUMBER_OF_OBJECTS = 2
+NUMBER_OF_OBJECTS = 10
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
 
 
 class Circle_Object:
+    id_list = []
+    COLORS = [(255,255,255), ]
     def __init__(self):
-        self.mass = randint(20, 100)
+        self.id = len(self.id_list) + 1
+
+        self.mass = randint(10, 100)
         self.radius = randint(10, 40)
-        self.gravity = 0.35
-        self.friction = -0.12
-        self.position = pygame.math.Vector2(randint(0+self.radius, WIDTH-self.radius), randint(0+self.radius, HEIGHT-self.radius))
-        self.velocity = pygame.math.Vector2(randint(-10, 10), randint(-10, 10))
-        self.acceleration = pygame.math.Vector2(1, 1)
-
-
-        self.x_0 = 0
-        self.y_0 = 0
-        self.vx_0 = [np.random.uniform(0.1, 50.0), np.random.uniform(0.1, 50.0)]
-        self.vy_0 = [np.random.uniform(0.1, 50.0), np.random.uniform(0.1, 50.0)]
+        self.pos = pygame.math.Vector2(randint(0+self.radius, WIDTH-self.radius), randint(0+self.radius, HEIGHT-self.radius))
+        self.v = pygame.math.Vector2(randint(-15, 15), randint(-15, 15))
+        if self.v.x == 0 or self.v.y == 0:
+            self.v = pygame.math.Vector2(randint(-15, 15), randint(-15, 15))
+        self.a = pygame.math.Vector2(1, 1)
+        self.color = (randint(0,255),randint(0,255),randint(0,255))
+        # self.gravity = 0.35
+        # self.friction = -0.12
 
     def draw(self, screen):
         self.screen = screen
-        pygame.draw.circle(self.screen,(255,255,255),(self.x_0, self.y_0), self.radius)
+        pygame.draw.circle(self.screen, self.color, (self.pos.x, self.pos.y), self.radius)
 
     def update(self, dt):
         self.horizontal_movement(dt)
         self.vertical_movement(dt)
 
-    def collision_check(self):
-        collision_x = self.position.x - self.radius
-        collision_y = self.position.y + self.radius
-        return collision_x, collision_y
-
     def horizontal_movement(self, dt):
         #self.velocity.x += self.acceleration.x*dt
         #self.limit_velocity(4)
         #self.position.x += self.velocity.x * dt + (0.5 * self.acceleration.x ) * (dt*dt)
-        self.position.x += self.velocity.x * dt
-        x, y = self.collision_check()
-        if x < 0 or WIDTH < x:
-            self.x_0 =(-1)*self.position.x
-        else:
-            self.x_0 = self.position.x
+        self.pos.x += self.v.x * dt
+        col_left, col_right, col_top, col_bottom = self.collision_check()
+        if col_left or col_right:
+            self.v.x *= -1
+        #elif col_top or col_bottom:
+         #   self.v.y *= -1
 
 
     def vertical_movement(self, dt):
         #self.velocity.y += self.acceleration.y * dt
         #self.limit_velocity(4)
         #self.position.y += self.velocity.y * dt + (0.5 * self.acceleration.y) * (dt * dt)
-        self.position.y += self.velocity.y * dt
-        self.y_0 = self.position.y
+        self.pos.y += self.v.y * dt
+        col_left, col_right, col_top, col_bottom = self.collision_check()
+        if col_top or col_bottom:
+            self.v.y *= -1
+
 
     def limit_velocity(self, max_v):
-        min(-max_v, max(self.velocity.x, max_v))
-        if abs(self.velocity.x < 0.1):
+        min(-max_v, max(self.v.x, max_v))
+        if abs(self.v.x < 0.1):
             self.velocity = 0.0
 
 
-    #def new_velocity(self):
-     #   vx = vx + ax*t
-     #   vy = vy + ay*t
+    def collision_check(self):
+        collision_left =  False
+        collision_right = False
+        collision_top = False
+        collision_bottom = False
+
+        if self.pos.x - self.radius <= 0.0:
+            collision_left = True
+        elif self.pos.x + self.radius >= WIDTH:
+            collision_right = True
+        elif self.pos.y - self.radius <= 0.0:
+            collision_top = True
+        elif self.pos.y + self.radius >= HEIGHT:
+            collision_bottom = True
+        return collision_left, collision_right, collision_top, collision_bottom
 
 
-circle1 = Circle_Object()
+    def add_text(self):
+        self.screen.blit(self.font.render('Hello!', True, (255, 0, 0)), (200, 100))
+        pygame.display.update()
+
+circles = [Circle_Object() for i in range(NUMBER_OF_OBJECTS)]
 
 def redraw_window(screen):
-    circle1.draw(screen)
-
-
+    screen.fill((0, 0, 0))
+    for i in circles:
+        i.draw(screen)
 
 def main():
     pygame.init()
+    font = pygame.font.SysFont(None, 30)
     scaling = 0.00001
     dt = clock.tick(60) * scaling * FPS
     running = True
+
 
 
     while running:
@@ -93,13 +110,15 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        circle1.update(dt)
+
         redraw_window(screen)
         pygame.display.flip()
+        for i in circles:
+            i.update(dt)
         clock.tick(FPS)
-
     pygame.quit()
     quit()
+
 
 if __name__ == '__main__':
     main()
